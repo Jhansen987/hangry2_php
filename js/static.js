576079accb0aa1f066b55a59bf6dd0cbc90777a3
Jhansen Ocado <jhansenreinier.ocado.cics@ust.edu.ph>
2023-09-08 19:09:27 +0800

    first

--- js/static.js
+++
@@ -1,8 +0,0 @@
-function responsiveNav(){
-	let x = document.getElementById('responsive-nav-menu');
-	let y = document.getElementById('black-screen');
-	x.classList.toggle('slide-down');
-	y.classList.toggle('fade-in');
-}
-
-document.getElementById('black-screen').addEventListener('click',responsiveNav);
